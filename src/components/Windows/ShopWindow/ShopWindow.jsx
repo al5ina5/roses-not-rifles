2a9754db53ebb68abb36e4e59545fe42febb543b
Sebastian Alsina <alsinas@me.com>
2020-12-31 16:11:20 -0500

    Added Window Content

--- src/components/Windows/ShopWindow/ShopWindow.jsx
+++ src/components/Windows/ShopWindow/ShopWindow.jsx
@@ -35,27 +35,10 @@ export default function ShopWindow({ close, desktopRef }) {
                         Checkout
                     </a>
                 </div>
-                // <div>
-                //     <Button className={isProducts ? 'underline' : ''} onClick={() => setActiveTab(PRODUCTS_TAB)}>
-                //         Products
-                //     </Button>
-                //     <Button className={isCheckout ? 'underline' : ''} onClick={() => setActiveTab(CHECKOUT_TAB)}>
-                //         Checkout
-                //     </Button>
-                // </div>
             }>
-            {/* <div className='fixed text-xs right flex flex-col mt-5 -ml-4 bg-win-gray'>
-                <Button className={isProducts ? 'underline' : ''} onClick={() => setActiveTab(PRODUCTS_TAB)}>
-                    Products
-                </Button>
-                <Button className={isCheckout ? 'underline' : ''} onClick={() => setActiveTab(CHECKOUT_TAB)}>
-                    Checkout
-                </Button>
-            </div> */}
-
-            {/* {isProducts && <ProductsTab />}
+            {isProducts && <ProductsTab />}
             {isCart && <CartTab />}
-            {isCheckout && <CheckoutTab />} */}
+            {isCheckout && <CheckoutTab />}
         </Window>
     )
 }
